550b9f8560163f93822911c5905cd81500464d61
camilomartinezf <camilomartinez@mbp-de-camilo.gateway.pace.com>
2015-02-11 17:41:10 -0600

    1

--- final/app/controllers/users_controller.rb
+++ final/app/controllers/users_controller.rb
@@ -12,25 +12,33 @@ class UsersController < ApplicationController
   end
 
   def new
-    
+    @users = User.new
   end
 
 
   def create
-    
+      users_params = params.require(:user).permit(:user_name)
+    User.create(users_params)
+    redirect_to users_path
   end
 
 
   def edit
+     @users = User.find_by(id: params["id"])
     
   end
 
 
   def update
-    
+    users_params = params.require(:user).permit(:user_name)
+    @users = User.find_by(id: params["id"])
+    @users.update(users_params)
+    redirect_to users_path
   end
 
   def destroy
-    
+    @users = User.find_by(id: params["id"])
+    @users.destroy
+    redirect_to users_path  
   end
 end
